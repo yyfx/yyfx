fc8277f2fcc94c73ba837b97beb913237ec1a7cc
yaoyifcci <yaoyi@fcci.cn>
2018-12-24 02:06:47 +0800

    autoremove

--- src/src/cli/CliBase.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace yyfx\cli;
-
-abstract class CliBase
-{
-    abstract public function run($args);
-}
--- src/src/index.sample.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'yyfx.php';
-$rule = [
-    'index' => 'Index'
-];
-yyfx\yyfx::App()->set('appNamespace', 'myApp')->set('routeRule', $rule)->run();
